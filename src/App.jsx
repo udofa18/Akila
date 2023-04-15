0182d9569ff69f20f128cce467914fac24b33909
Ufofa18 <danieludofa452@gmail.com>
2023-04-15 12:09:17 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,24 @@ const people = [
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
+  {
+    name: 'Leslie Alexander',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Leslie Alexander',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Leslie Alexander',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
   // More people...
 ]
 export default function App() {
@@ -254,40 +272,39 @@ export default function App() {
         </div>
       </section>
       <section className="w-full h-50 mb-8 h-full  rounded-3xl p-10 text-center  bg-orange-400">
-        <div>
-          <h1 className=" text-black bg p-5 text-center text-3xl">
-            Meet the Team:
-          </h1>
-          <div className="bg-white py-24 sm:py-32">
-            <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
-              <div className="max-w-2xl">
+        
+        <div className="max-w-2xl m-auto">
                 <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                  Meet our leadership
+                  Meet our team:
                 </h2>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
+                <p className="mt-6 text-lg leading-8 text-white">
                   Libero fames augue nisl porttitor nisi, quis. Id ac elit odio
                   vitae elementum enim vitae ullamcorper suspendisse.
                 </p>
-              </div>
+            </div>
+          <div className=" py-24 sm:py-32 ">
+            <div className="mx-auto grid m-auto gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+              
               <ul
                 role="list"
-                className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
+                className="grid gap-x-8 gap-y-12 sm:grid-cols-4 sm:gap-y-16 xl:col-span-4"
               >
                 {people.map((person) => (
                   <li key={person.name}>
                     <div className="flex items-center gap-x-6">
                       <img
-                        className="h-16 w-16 rounded-full"
+                        className="h-40 w-40 rounded-full"
                         src={person.imageUrl}
                         alt=""
                       />
-                      <div>
+                      <div className="">
                         <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
                           {person.name}
                         </h3>
                         <p className="text-sm font-semibold leading-6 text-indigo-600">
                           {person.role}
                         </p>
+                       
                       </div>
                     </div>
                   </li>
@@ -295,7 +312,7 @@ export default function App() {
               </ul>
             </div>
           </div>
-        </div>
+        
       </section>
     </>
   );
